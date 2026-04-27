d1fcf55993f66cad4d0fdb7f3e3399c5933351c9
kumar3000 <sidarthoscar15@gmail.com>
2026-04-26 21:02:59 -0400

    cargo new TFL

---
+++ TFL/target/debug/TFL.d
@@ -0,0 +1,1 @@
+C:\Users\sidar\Documents\Code\GITHUB\TFL\TFL\target\debug\TFL.exe: C:\Users\sidar\Documents\Code\GITHUB\TFL\TFL\src\main.rs
---
+++ TFL/target/debug/deps/TFL.d
@@ -0,0 +1,5 @@
+C:\Users\sidar\Documents\Code\GITHUB\TFL\TFL\target\debug\deps\TFL.d: src\main.rs
+
+C:\Users\sidar\Documents\Code\GITHUB\TFL\TFL\target\debug\deps\TFL.exe: src\main.rs
+
+src\main.rs:
